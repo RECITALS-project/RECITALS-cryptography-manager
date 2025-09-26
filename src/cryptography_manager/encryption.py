ee43650e9da8e21f19ce9eb08ed6c78f6fc893c3
Konstantinos Chousos <K.chousos@protonmail.com>
2025-09-26 10:19:51 +0300

    update

--- src/cryptography_manager/encryption.py
+++ src/cryptography_manager/encryption.py
@@ -30,10 +30,10 @@ class Encryption:
         """
         Generates a key for AEAD primitive, using AES256_GCM_SIV implementation.
 
-        > AES128_GCM_SIV is nearly as fast as AES128_GCM. It has the same limits as
+        > *AES128_GCM_SIV is nearly as fast as AES128_GCM. It has the same limits as
         > AES128_GCM on the number of messages and message size, but when these
         > limits are exceeded, it fails in a less catastrophic way: it may only leak
         > the fact that two messages are equal. This makes it safer to use than
-        > AES128_GCM, but it is less widely used in practice.
+        > AES128_GCM, but it is less widely used in practice.*
         """
         ...
